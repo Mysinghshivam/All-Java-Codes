2cb6448440d2f2b7f3232ac6e9be5a482c1f7f67
Mysinghshivam <mysinghshivam@gmail.com>
2022-12-13 21:21:34 +0530

    13/12/2022 I have learnt circular Queue and push at Git up.

---
+++ workspace for java/Stack and Queues/src/circularQueue.java
@@ -0,0 +1,77 @@
+public class circularQueue {
+
+    static class CQueue {
+        static int size;
+        static int arr[];
+        static int front = -1;
+        static int rear = -1;
+
+        CQueue(int n) {
+            arr = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty() {
+            return front == -1 && rear == -1;
+        }
+
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+
+        //add data in circular queue
+        public void push(int data) {
+            if (isFull()) {
+                System.out.print("Queue full");
+                return;
+            }
+            //for 1st element
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        //enqueue data O(1)
+        public int pop() {
+            if (isEmpty()) {
+                System.out.print("Queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+            if (front == rear) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        //peek value
+        public int peek() {
+            if (isEmpty()) {
+                System.out.print("Queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        CQueue queue = new CQueue(5);
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        queue.push(5);
+        System.out.print(queue.pop());
+        queue.push(6);
+        System.out.print(queue.pop());
+        queue.push(7);
+        while (!queue.isEmpty()){
+            System.out.print(queue.peek());
+            queue.pop();
+        }
+    }
+}
