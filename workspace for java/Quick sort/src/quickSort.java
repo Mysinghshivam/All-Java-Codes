aaa6a7cf37e265e159f318db96175f3f76a9c0c6
Mysinghshivam <mysinghshivam@gmail.com>
2023-03-11 23:02:34 +0530

    27/02/2022 revision of quick sort

--- workspace for java/Quick sort/src/quickSort.java
+++ workspace for java/Quick sort/src/quickSort.java
@@ -1,3 +1,4 @@
+/*
 public class quickSort {
     public static int partition(int [] arr, int low, int high) {
         int pivot = arr[high];
@@ -33,6 +34,42 @@ public class quickSort {
         for (int i = 0; i<n; i++){
             System.out.print(arr[i] + " ");
         }
+    }
+}
+*/
+class quick_sort{
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int x = low-1;
+        for (int i=low; i<high; i++){
+            if (arr[i] < pivot){
+                x++;
+                int temp = arr[x];
+                arr[x] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        x++;
+        int temp = arr[x];
+        arr[x] = pivot;
+        arr[high] = temp;
+        return x;
+    }
+
+      public static void quick(int [] arr, int low, int high){
+        if (low < high) {
+            int pidx = partition(arr, low, high);
+            quick(arr, low, pidx - 1);
+            quick(arr, pidx+1, high);
+        }
+      }
+    public static void main(String[] args) {
+     int [] arr = {6, 2, 8, 5, 1, 4};
+     int n = arr.length;
+     quick(arr, 0, n-1);
+     for (int i =0; i<n; i++){
+         System.out.print(arr[i] + " ");
+     }
     }
 }
 
